54b2eae09c7073693810da5f171baced286202a4
Dave Charnuska <davecharnuska@gmail.com>
2016-06-05 12:58:47 -0400

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var reload = browserSync.reload
+
+gulp.task('serve', () => {
+  browserSync.init({
+    server: './'
+  })
+  gulp.watch('css/main.css').on('change', reload)
+  gulp.watch('index.html').on('change', reload)
+  gulp.watch('js/scripts.js').on('change', reload)
+})
